ebc0b5ad885a77d6cb393a695a9cdeff515c5722
Sirisha Ongole <40199915+Sirisha-Ongole@users.noreply.github.com>
2023-08-14 17:15:46 -0400

    pomotimer cal working

--- resources/js/components/Task/PomoView.js
+++ resources/js/components/Task/PomoView.js
@@ -1,107 +1,97 @@
-import React, { createRef, useRef } from 'react';
-import { PomoTimer } from './PomoTimer';
-import { useSelector } from 'react-redux';
-import { Button, Modal,istGroup,Col,Tab,Row,ListGroupItem,Card,Container,Tabs,Table} from "react-bootstrap";
-import { CountdownApi } from 'react-countdown';
-import Countdown from 'react-countdown';
+import React, { createRef, useRef, useEffect } from "react";
+import { PomoTimer } from "./PomoTimer";
+import { useSelector } from "react-redux";
+import {
+    Button,
+    Modal,
+    istGroup,
+    Col,
+    Tab,
+    Row,
+    ListGroupItem,
+    Card,
+    Container,
+    Tabs,
+    Table,
+} from "react-bootstrap";
+import { CountdownApi } from "react-countdown";
+import Countdown from "react-countdown";
 import { rendertime } from "./PomoTimerFormat";
-import { set } from 'lodash';
+import { set } from "lodash";
 
 export const PomoView = () => {
-    
     var CountdownApi;
-    const handleResetClick = (tabname) => {
-       if(tabname.name == "pomodoro"){
-        console.log(setRefpomo);
-        setRefpomo.current.getApi();
-        setRefpomo.current.stop();
-       }else{
-        setRefshort.current.getApi();
-        setRefshort.current.stop();
-       }
+    const handleResetClick = (e) => {
+        console.log(e.target.value);
+        tabRef.current[e.target.value].current.getApi();
+        tabRef.current[e.target.value].current.stop();
+    };
+    const handleStartClick = (e, i) => {
+        console.log(e.target.value);
+        tabRef.current[e.target.value].current.getApi();
+        tabRef.current[e.target.value].current.start();
+    };
 
-    // setRefNew.current.getApi();
-    // setRefNew.current.stop();
-    }
-    const handleStartClick = (tabname) => {    
-        if(tabname.name == "pomodoro"){
-            setRefpomo.current.getApi();
-            setRefpomo.current.start();
-           }else{
-            setRefshort.current.getApi();
-            setRefshort.current.start();
-           }
-    }
+    console.log("test");
 
+    //const currentPomoDetails = useSelector((state) => state.currentTaskReducer.taskDetails.pomodoro_template.attributes);
 
-      const currentPomoDetails = useSelector((state) => state.currentTaskReducer.taskDetails.pomodoro_template.attributes);    
-      console.log(currentPomoDetails);
-      const setRefpomo = useRef();
-      const setRefshort = useRef();
+    //write a filter function to filter out the pomodoro_id from the currentPomoDetails
+    const currentPomoDetails = useSelector(
+        (state) =>
+            state.currentTaskReducer.taskDetails.pomodoro_template.attributes
+    );
+    console.log(currentPomoDetails);
+    const filteredPomoDetails =
+        currentPomoDetails &&
+        Object.entries(currentPomoDetails).filter(
+            (pomo, i) => pomo[0] != "pmodoro_id" && pomo[0] != "cycle_count"
+        );
 
-    //   const setRefNew = useRef();
-    //   setRefNew.current = currentPomoDetails && Object.entries(currentPomoDetails).map(([key, value]) => {  
-    //     if (key != "id" && key != "cycle_count" && key != "pomodoro_id") {
-    //         (setRefNew.current && setRefNew.current[key]) ?? React.createRef(); 
-    //     } 
-    // });
+    const tabRef = useRef([]);
+    tabRef.current =
+    filteredPomoDetails &&
+    filteredPomoDetails.map(
+            (names, i) =>
+                (tabRef.current && tabRef.current[i]) ?? React.createRef()
+        );
+
+    console.log(tabRef.current);
 
     return (
-    <>
-    <Tabs defaultActiveKey="pomodoro" id="uncontrolled-tab-example" className="mb-3">
-                <Tab eventKey="pomodoro" title="Pomodoro">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-12">
-                                <h1 className="text-center">Pomodoro Timer</h1>
+        <>
+          <Tabs defaultActiveKey="cycle_time" id="uncontrolled-tab-example" className="mb-3">
+            {filteredPomoDetails &&
+               filteredPomoDetails.map((names, i) => (
+                    <Tab eventKey={names[0]} title={names[0]}>
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-12">
+                                    <h1 className="text-center">Pomodoro Timer</h1>
+                                </div>
                             </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="text-center">
-                                    <h1>
-                                        <Countdown date={Date.now() + 1.2e+6} renderer={rendertime} autoStart={false} ref={setRefNew}/>
-                                    </h1>
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="text-center">
+                                        <h1>
+                                        <Countdown date={Date.now() + 1.2e6} renderer={rendertime} autoStart={false} ref={tabRef.current[i]} />
+                                        </h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="text-center">
+                                    <Button value={i} onClick={handleStartClick} variant="primary" className="m-2">Start</Button>
+                                    <Button value={i} onClick={handleResetClick} variant="secondary" className="m-2">Rest</Button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="text-center">
-                                    <Button onClick={(e) => handleStartClick({ name: e.target.value })} variant="primary" className="m-2" value="pomodoro">Start</Button>
-                                    <Button onClick={(e) => handleResetClick({ name: e.target.value })} variant="secondary" className="m-2" value="pomodoro">Reset</Button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Tab>
-                <Tab eventKey="short time" title="Short Time">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-12">
-                                <h1 className="text-center">Short Time Timer</h1>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="text-center">
-                                    <h1>
-                                    <Countdown date={Date.now() + 1.2e+6} renderer={rendertime} autoStart={false} ref={setRefNew}/>
-                                    </h1>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="text-center">
-                                    <Button onClick={(e) => handleStartClick({ name: e.target.value })} variant="primary" className="m-2" value="shorttime">Start</Button>
-                                    <Button onClick={(e) => handleResetClick({ name: e.target.value })} variant="secondary" className="m-2" value="shorttime">Reset</Button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Tab></Tabs>
-    
-    </>
-  )
-}
+                    </Tab>
+                ))}
+                </Tabs>
+        </>
+    );
+
+    };
