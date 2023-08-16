2f4be05c7676acc61200a6d0efd1d583d1a5f5da
Sirisha Ongole <40199915+Sirisha-Ongole@users.noreply.github.com>
2023-08-16 16:41:21 -0400

    looping the filtredpomoDetaiils Code

--- resources/js/components/Task/PomoView.js
+++ resources/js/components/Task/PomoView.js
@@ -1,61 +1,33 @@
-import React, { createRef, useRef, useEffect } from "react";
-import { PomoTimer } from "./PomoTimer";
+import React, { createRef, useRef } from "react";
 import { useSelector } from "react-redux";
-import {
-    Button,
-    Modal,
-    istGroup,
-    Col,
-    Tab,
-    Row,
-    ListGroupItem,
-    Card,
-    Container,
-    Tabs,
-    Table,
-} from "react-bootstrap";
-import { CountdownApi } from "react-countdown";
+import { Button,Tab,Tabs} from "react-bootstrap";
 import Countdown from "react-countdown";
 import { rendertime } from "./PomoTimerFormat";
-import { set } from "lodash";
+import { store } from "../../Redux/store";
 
 export const PomoView = () => {
-    var CountdownApi;
+
     const handleResetClick = (e) => {
-        console.log(e.target.value);
         tabRef.current[e.target.value].current.getApi();
         tabRef.current[e.target.value].current.stop();
     };
-    const handleStartClick = (e, i) => {
-        console.log(e.target.value);
+    const handleStartClick = (e) => {
         tabRef.current[e.target.value].current.getApi();
         tabRef.current[e.target.value].current.start();
     };
 
-    console.log("test");
+    const currentPomoDetails = useSelector((state) => state.currentTaskReducer.taskDetails.pomodoro_template.attributes);
+    const CurrentTaskDetails = useSelector((state) => state.currentTaskReducer.taskDetails);    
 
-    //const currentPomoDetails = useSelector((state) => state.currentTaskReducer.taskDetails.pomodoro_template.attributes);
 
-    //write a filter function to filter out the pomodoro_id from the currentPomoDetails
-    const currentPomoDetails = useSelector(
-        (state) =>
-            state.currentTaskReducer.taskDetails.pomodoro_template.attributes
-    );
-    console.log(currentPomoDetails);
-    const filteredPomoDetails =
-        currentPomoDetails &&
-        Object.entries(currentPomoDetails).filter(
-            (pomo, i) => pomo[0] != "pmodoro_id" && pomo[0] != "cycle_count"
-        );
+    let filteredPomoDetails = currentPomoDetails && Object.entries(currentPomoDetails).
+    filter((pomo, i) => pomo[0] != "pmodoro_id" && pomo[0] != "cycle_count");
+
+    filteredPomoDetails = filteredPomoDetails && filteredPomoDetails.map((names, i) => [names[0], names[1] * 60000]);
+    console.log(filteredPomoDetails);
 
     const tabRef = useRef([]);
-    tabRef.current =
-    filteredPomoDetails &&
-    filteredPomoDetails.map(
-            (names, i) =>
-                (tabRef.current && tabRef.current[i]) ?? React.createRef()
-        );
-
+    tabRef.current = filteredPomoDetails && filteredPomoDetails.map((names, i) => (tabRef.current && tabRef.current[i]) ?? React.createRef());
     console.log(tabRef.current);
 
     return (
@@ -67,14 +39,14 @@ export const PomoView = () => {
                         <div className="container-fluid">
                             <div className="row">
                                 <div className="col-12">
-                                    <h1 className="text-center">Pomodoro Timer</h1>
+                                    <h1 className="text-center">{CurrentTaskDetails.body}</h1>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col-12">
                                     <div className="text-center">
                                         <h1>
-                                        <Countdown date={Date.now() + 1.2e6} renderer={rendertime} autoStart={false} ref={tabRef.current[i]} />
+                                        <Countdown date={Date.now() + names[1]} renderer={rendertime} autoStart={false} ref={tabRef.current[i]} />
                                         </h1>
                                     </div>
                                 </div>
@@ -93,5 +65,4 @@ export const PomoView = () => {
                 </Tabs>
         </>
     );
-
     };
